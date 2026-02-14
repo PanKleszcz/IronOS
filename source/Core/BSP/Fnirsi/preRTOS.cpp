487ea460628c4ee7eb77539774085fcbbd544f5a
PanKleszcz <pan@klesz.cz>
2026-02-14 12:07:13 +0100

    feat: add basic FreeRTOS facilities

---
+++ source/Core/BSP/Fnirsi/preRTOS.cpp
@@ -0,0 +1,29 @@
+/*
+ * preRTOS.c
+ *
+ *  Created on: 29 May 2020
+ *      Author: Ralim
+ */
+
+#include "BSP.h"
+// #include "I2CBB1.hpp"
+// #include "I2CBB2.hpp"
+// #include "Pins.h"
+#include "Setup.h"
+// #include "configuration.h"
+// #include <I2C_Wrapper.hpp>
+#include "n32l40x_rcc.h"
+#include "n32l40x_wwdg.h"
+#include "main.hpp"
+#include "OLED.hpp"
+
+void preRToSInit() {
+  __enable_irq(); // Fnirsi bootloader disables interrupts before jumping to the user's app
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick.
+   */
+  // hwInit();
+//   HAL_Init();
+//   Setup_HAL(); // Setup all the HAL objects
+  BSPInit();
+}
