0cc44cdc208eccb3907f0803fcbb50db4a48690a
OK2CM <martin@ok2cm.cz>
2026-06-04 22:25:49 +0200

    fix: Debug menu layout.

--- source/Core/Threads/UI/drawing/color_160x80/draw_debug_menu.cpp
+++ source/Core/Threads/UI/drawing/color_160x80/draw_debug_menu.cpp
@@ -13,6 +13,8 @@ void ui_draw_debug_menu(const uint8_t item_number) {
   Display::print(SmallSymbolVersionNumber, FontStyle::SMALL); // Print version number
   Display::setCursor(0, 24);                                  // second line
   Display::print(DebugMenu[item_number], FontStyle::SMALL);
+  Display::setCursor(LCD_WIDTH - 6*FONT_SMALL_WIDTH, 48);     // third line
+  uint16_t tmp;
   switch (item_number) {
   case 0: // Build Date
     break;
@@ -27,55 +29,61 @@ void ui_draw_debug_menu(const uint8_t item_number) {
     Display::drawHex(getDeviceValidation(), FontStyle::SMALL, 8);
     Display::setCursor(0, 16); // second line
 #endif
+    Display::setCursor(36, 24);
     Display::drawHex((uint32_t)(id >> 32), FontStyle::SMALL, 8);
-    Display::setCursor(36, 40);
+    Display::setCursor(36, 48);
     Display::drawHex((uint32_t)(id & 0xFFFFFFFF), FontStyle::SMALL, 8);
   } break;
   case 2: // ACC Type
+    Display::setCursor(0, 48);
     Display::print(AccelTypeNames[(int)DetectedAccelerometerVersion], FontStyle::SMALL);
     break;
   case 3: // Power Negotiation Status
+    Display::setCursor(0, 48);
     Display::print(PowerSourceNames[getPowerSourceNumber()], FontStyle::SMALL);
     break;
   case 4: // Input Voltage
+    Display::print(SmallSymbolSpace, FontStyle::SMALL);
+    Display::print(SmallSymbolSpace, FontStyle::SMALL);
     printVoltage();
     break;
   case 5: // Temp in °C
-    Display::printNumber(TipThermoModel::getTipInC(), 6, FontStyle::SMALL);
+    Display::printNumber(TipThermoModel::getTipInC(), 5, FontStyle::SMALL);
     break;
   case 6: // Handle Temp in °C
-    Display::printNumber(getHandleTemperature(0) / 10, 6, FontStyle::SMALL);
+    tmp = getHandleTemperature(0);
+    Display::printNumber(tmp / 10, 4, FontStyle::SMALL);
     Display::print(SmallSymbolDot, FontStyle::SMALL);
-    Display::printNumber(getHandleTemperature(0) % 10, 1, FontStyle::SMALL);
+    Display::printNumber(tmp % 10, 1, FontStyle::SMALL);
     break;
   case 7: // Max Temp Limit in °C
-    Display::printNumber(TipThermoModel::getTipMaxInC(), 6, FontStyle::SMALL);
+    Display::printNumber(TipThermoModel::getTipMaxInC(), 5, FontStyle::SMALL);
     break;
   case 8: // System Uptime
-    Display::printNumber(xTaskGetTickCount() / TICKS_100MS, 8, FontStyle::SMALL);
+    Display::printNumber(xTaskGetTickCount() / TICKS_100MS, 5, FontStyle::SMALL);
     break;
   case 9: // Movement Timestamp
-    Display::printNumber(lastMovementTime / TICKS_100MS, 8, FontStyle::SMALL);
+    Display::printNumber(lastMovementTime / TICKS_100MS, 5, FontStyle::SMALL);
     break;
-  case 10:                                                                 // Tip Resistance in Ω
-    Display::printNumber(getTipResistanceX10() / 10, 6, FontStyle::SMALL); // large to pad over so that we cover ID left overs
+  case 10: // Tip Resistance in Ω
+    Display::printNumber(getTipResistanceX10() / 10, 4, FontStyle::SMALL);
     Display::print(SmallSymbolDot, FontStyle::SMALL);
     Display::printNumber(getTipResistanceX10() % 10, 1, FontStyle::SMALL);
     break;
   case 11: // Raw Tip in µV
-    Display::printNumber(TipThermoModel::convertTipRawADCTouV(getTipRawTemp(0), true), 8, FontStyle::SMALL);
+    Display::printNumber(TipThermoModel::convertTipRawADCTouV(getTipRawTemp(0), true), 5, FontStyle::SMALL);
     break;
   case 12: // Tip Cold Junction Compensation Offset in µV
-    Display::printNumber(getSettingValue(SettingsOptions::CalibrationOffset), 8, FontStyle::SMALL);
+    Display::printNumber(getSettingValue(SettingsOptions::CalibrationOffset), 5, FontStyle::SMALL);
     break;
   case 13: // High Water Mark for GUI
-    Display::printNumber(uxTaskGetStackHighWaterMark(GUITaskHandle), 8, FontStyle::SMALL);
+    Display::printNumber(uxTaskGetStackHighWaterMark(GUITaskHandle), 5, FontStyle::SMALL);
     break;
   case 14: // High Water Mark for Movement Task
-    Display::printNumber(uxTaskGetStackHighWaterMark(MOVTaskHandle), 8, FontStyle::SMALL);
+    Display::printNumber(uxTaskGetStackHighWaterMark(MOVTaskHandle), 5, FontStyle::SMALL);
     break;
   case 15: // High Water Mark for PID Task
-    Display::printNumber(uxTaskGetStackHighWaterMark(PIDTaskHandle), 8, FontStyle::SMALL);
+    Display::printNumber(uxTaskGetStackHighWaterMark(PIDTaskHandle), 5, FontStyle::SMALL);
     break;
     break;
 #ifdef HALL_SENSOR
